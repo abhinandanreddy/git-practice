800c58c7918e09c863fd0dda059222f9c4ba3d92
Abhinandan Mandala <abhinandhanrdd4@gmail.com>
2026-02-25 19:26:06 -0500

    abhi rdd4 merge 1st commit

--- example1.ts
+++ example1.ts
@@ -1,8 +1,1 @@
-function ex2() { }
-<<<<<<< HEAD
-"abhi reddi 1 change"
-"abhi reddi 2nd change"
-=======
-"a new line added by abhireddi2"
-"abhi rdd4"
->>>>>>> abhi rdd4 changes
+abhi rdd4 changes
